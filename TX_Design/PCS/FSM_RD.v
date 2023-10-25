ec5c3f9454625594649439cda1cf040c41e312c9
al719 <88440404+al719@users.noreply.github.com>
2023-10-25 23:32:39 +0300

    Update FSM_RD.v

--- TX_Design/PCS/FSM_RD.v
+++ TX_Design/PCS/FSM_RD.v
@@ -1,15 +1,24 @@
 module FSM_RD(
-				input wire[7:0] data_neg,
-				input wire[7:0] data_pos,
+				input wire[9:0] data_neg,
+				input wire[9:0] data_pos,
 				input Bit_Rate_10,
-				output reg[9:0] Data_10
+				input Rst,
+				output reg[9:0] Data_10,
+				output reg enable_PMA
 				);
 
 localparam RD_neg = 1'b0,
 		   RD_pos = 1'b1;
-reg current_state , next_state;
-always @(posedge Bit_Rate_10) begin
-	current_state <= next_state;
+reg current_state , next_state , pos_flag;
+always @(posedge Bit_Rate_10 , negedge Rst) begin
+	if(!Rst) begin 
+		if(pos_flag)
+			current_state <= RD_pos; 
+		else 
+			current_state <= RD_neg;
+	end 
+	else 
+		current_state <= next_state;
 end
 
 always @(*) begin
@@ -21,9 +30,16 @@ always @(*) begin
 end
 
 always @(*) begin
+	enable_PMA = 1'b1;
 	case (current_state)
-		RD_neg : Data_10 = data_neg;
-		RD_pos : Data_10 = data_pos;
+		RD_neg : begin 
+			Data_10 = data_neg;
+			pos_flag = 1'b1;
+		end    
+		RD_pos : begin 
+			Data_10 = data_pos;
+			pos_flag = 1'b0;
+		end 
 		default : Data_10 = data_neg;
 	endcase
 end
