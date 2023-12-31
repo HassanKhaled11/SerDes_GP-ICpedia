0831b3f011bfb4587b58aecdf7159a1573befd59
Abdelmagid Mohamed <abdelmagidmohamed89@yahoo.com>
2023-12-31 12:37:33 +0200

    corrected Receiver status

--- RX-Design/ReceiverStatus/Receiver_Status.v
+++ RX-Design/ReceiverStatus/Receiver_Status.v
@@ -14,13 +14,14 @@ module Reeceiver_Status (
       6'b00_00_00: RxStatus <= 3'b000;
       6'b00_10_00: RxStatus <= 3'b001;
       6'b00_01_00: RxStatus <= 3'b010;
+      6'b11_11_11: RxStatus <= 3'b011;  /////receiver detected ignored
+      //6'b00_xx_00: RxStatus <= 3'b011;
 
-     //6'b00_xx_00: RxStatus <= 3'b011;
-
+      6'b00_xx_11: RxStatus <= 3'b100;
       6'b10_00_00: RxStatus <= 3'b101;
       6'b01_00_00: RxStatus <= 3'b110;
       6'b00_xx_01: RxStatus <= 3'b111;
-      default    : RxStatus <= 3'b000;
+      default:     RxStatus <= 3'b000;
     endcase
 
   end
