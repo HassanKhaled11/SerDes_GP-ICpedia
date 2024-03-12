66a4c455d2b916b60ae0c6f5751e751f2c8eb69e
Abdelmagid Mohamed <abdelmagidmohamed89@yahoo.com>
2024-03-12 17:21:47 +0200

    made channel more realistic

--- RX-Design/Channel/channel.sv
+++ RX-Design/Channel/channel.sv
@@ -3,15 +3,19 @@ module channel (
     input clk,
     input rst_n,
     input in,
-    output reg out
+    output real out
 );
+
   reg faster_clk;
+  parameter FASTER_CLK_PERIOD = 0.00019;
+  parameter NUMBER_OF_LEVELS = 256;
+  parameter THRESHOLD = NUMBER_OF_LEVELS / 2;
   // parameter precision = 5;
-  reg [10-1:0] out_fading;
+  reg [8-1:0] out_fading;
 
   initial begin
     faster_clk = 0;
-    forever #(5) faster_clk = ~faster_clk;
+    forever #(FASTER_CLK_PERIOD / 2) faster_clk = ~faster_clk;
   end
   always @(posedge faster_clk or negedge rst_n) begin
     if (!rst_n) begin
@@ -19,15 +23,15 @@ module channel (
       out <= 0;
     end else begin
       if (in == 1) begin
-        if (out_fading < 10'b1111_1111) out_fading <= out_fading + 1;
-        else out_fading = 10'b1111_1111;
+        if (out_fading < NUMBER_OF_LEVELS - 1) out_fading <= out_fading + 1;
+        else out_fading = NUMBER_OF_LEVELS - 1;
       end else begin
         if (out_fading > 0) out_fading <= out_fading - 1;
         else out_fading <= 0;
       end
     end
+    out = out_fading / NUMBER_OF_LEVELS;
   end
-
 endmodule
 
 `timescale 1ns / 1fs
@@ -35,18 +39,22 @@ module channel_Tb;
   reg clk;
   reg rst_n;
   reg in;
-  wire out;
+  real out;
 
   reg [19:0] inputVal;
 
   // Clock generation
   initial begin
     clk = 0;
-    forever #50 clk = ~clk;
+    forever #0.1 clk = ~clk;
   end
 
   // Instantiate the channel module
-  channel channel_inst (
+  channel #(
+      .NUMBER_OF_LEVELS(256),
+      .THRESHOLD(128),
+      .FASTER_CLK_PERIOD(0.0019)  //0.00019 will be always correct
+  ) channel_inst (
       .clk(clk),
       .rst_n(rst_n),
       .in(in),
@@ -57,25 +65,26 @@ module channel_Tb;
   initial begin
     // Reset
     rst_n = 0;
-    #100;
+    #5;
     rst_n = 1;
 
-    inputVal = 20'b1111_0101_1100_1000_1010;
-
     in = 1;
-    #100;
+    repeat (10) @(negedge clk);
+
+
     // Provide stimuli
     for (int i = 0; i < 1000; i = i + 1) begin
-      #1000 in = inputVal[i%20];
+      @(negedge clk) in = $random();
     end
 
-    #150 $stop;  // Stop simulation after 150 time units
+    #10 $stop;  // Stop simulation after 150 time units
   end
 
   // Monitor
   always @(posedge clk) begin
-    $display("Time = %0t, in = %b, out = %b", $time, in, out);
+    $display("Time = %0t, in = %b, out = %f", $time, in, out);
   end
 
 endmodule
 
+// module edgeDetector(input )
