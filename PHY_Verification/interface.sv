67657313eedd7ec4ba8076cc5cf6830f22025466
Hassan khaled <hassankhaled006@gmail.com>
2024-05-29 18:46:46 +0300

    SIGNALS PREPARATIONS PASSIVE AGENTS

--- PHY_Verification/interface.sv
+++ PHY_Verification/interface.sv
@@ -45,3 +45,92 @@ interface INTERNALS_if;
   bit         Disparity_Error;
 
 endinterface
+
+
+interface PASSIVE_if;
+
+////////////// TX_GASKET /////////////////
+
+  logic                                 tx_gasket_PCLK                 ;
+  logic                                 tx_gasket_Bit_Rate_CLK_10      ;
+  logic                                 tx_gasket_Reset_n              ;
+  logic  [5  : 0 ]                      tx_gasket_DataBusWidth         ;
+  logic  [31 : 0 ]                      tx_gasket_MAC_TX_Data          ;
+  logic  [3  : 0 ]                      tx_gasket_MAC_TX_DataK         ;
+  logic                                 tx_gasket_MAC_Data_En          ; 
+  logic                                 tx_gasket_TxDataK              ;
+  logic  [7  : 0 ]                      tx_gasket_TxData               ;
+
+////////////// ENCODER /////////////////
+
+  logic  [7:0]                          encoder_data                 ;
+  logic                                 encoder_MAC_Data_En          ;
+  logic                                 encoder_Bit_Rate_10          ;
+  logic                                 encoder_Rst                  ;
+  logic                                 encoder_TXDataK              ; 
+  logic  [9:0]                          encoder_data_out             ;
+
+////////////// TX_PMA /////////////////
+
+  logic                                 tx_pma_Bit_Rate_Clk_10      ;  
+  logic                                 tx_pma_Bit_Rate_Clk         ;
+  logic                                 tx_pma_Rst_n                ;
+  logic [10:0]                          tx_pma_Data_in              ; 
+  logic                                 tx_pma_MAC_Data_En          ; 
+  logic                                 tx_pma_TX_Out_P             ;   
+  logic                                 tx_pma_TX_Out_N             ;
+
+////////////// RX_S2p /////////////////
+
+  logic                                 rx_s2p_Recovered_Bit_Clk    ;
+  logic                                 rx_s2p_Ser_in               ;
+  logic                                 rx_s2p_Rst_n                ;
+  logic                                 rx_s2p_RxPolarity           ;
+  logic [9:0]                           rx_s2p_Data_Collected       ;
+
+////////////// RX_CDR /////////////////
+
+  logic                                 cdr_rst_n                   ;  
+  logic                                 cdr_clk_0                   ;
+  logic                                 cdr_clk_data                ;  
+  logic                                 cdr_Din                     ;
+  logic                                 cdr_PI_Clk                  ;
+  logic                                 cdr_Dout                    ;
+  //...internal 
+  logic [10:0]                          cdr_code                    ;
+
+////////////// EBUFFER /////////////////
+
+  logic                                 ebuffer_write_clk            ;
+  logic                                 ebuffer_read_clk             ;
+  logic [10:0]                          ebuffer_data_in              ;
+  logic                                 ebuffer_rst_n                ;
+  logic                                 ebuffer_skp_added            ;
+  logic                                 ebuffer_Skp_Removed          ;
+  logic                                 ebuffer_overflow             ;
+  logic                                 ebuffer_underflow            ;
+  logic [10:0]                          ebuffer_data_out             ;
+
+
+////////////// DECODER /////////////////
+
+  logic                                 decoder_CLK                  ;
+  logic                                 decoder_Rst_n                ;
+  logic [9:0]                           decoder_Data_in              ;
+  logic [7:0]                           decoder_Data_out             ;
+  logic                                 decoder_DecodeError          ;
+  logic                                 decoder_DisparityError       ;
+  logic                                 decoder_RxDataK              ;
+
+////////////// RX_GASKET ///////////////
+
+  logic                                 rx_gasket_clk_to_get           ;
+  logic                                 rx_gasket_PCLK                 ;
+  logic                                 rx_gasket_Rst_n                ;
+  logic                                 rx_gasket_Rx_Datak             ;
+  logic [ 5:0]                          rx_gasket_width                ;
+  logic [ 7:0]                          rx_gasket_Data_in              ;
+  logic [31:0]                          rx_gasket_Data_out             ;
+
+
+endinterface  
--- PHY_Verification/top.sv
+++ PHY_Verification/top.sv
@@ -17,6 +17,7 @@ module top;
 
   BFM_if dut_if ();
   INTERNALS_if internals_if ();
+  PASSIVE_if passive_if();
 
 
   /////////////////////////////////////
@@ -50,7 +51,6 @@ module top;
   assign internals_if.PCLK             = DUT.PCLK;
   assign internals_if.DataBusWidth     = DUT.DataBusWidth;
   assign internals_if.TX_Out_P         = DUT.PMA_U.TX_Out_P;
- // assign internals_if.Clk_offset       = DUT.PMA_U.Bit_Rate_Clk_offset;
   assign internals_if.recovered_clk_5G = DUT.PMA_U.recovered_clk_5G;
   assign internals_if.MAC_Data_En      = DUT.MAC_Data_En;
   assign internals_if.Decode_Error     = DUT.PCS_U.PCS_RX_U.DecodeError;
@@ -75,9 +75,101 @@ module top;
       .TX_Out_P    (DUT.PMA_U.TX_Out_P)
 
   );
-  ///////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////// PHY Assertions //////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////
+//////////// PASSIVE_AGENT_SIGNALS //////
+/////////////////////////////////////////
+
+
+////////////// TX_GASKET /////////////////
+
+  assign passive_if.tx_gasket_PCLK            = DUT.PCS_U.PCS_TX_U.GasKet_U.PCLK;
+  assign passive_if.tx_gasket_Bit_Rate_CLK_10 = DUT.PCS_U.PCS_TX_U.GasKet_U.Bit_Rate_CLK_10;
+  assign passive_if.tx_gasket_Reset_n         = DUT.PCS_U.PCS_TX_U.GasKet_U.Reset_n;
+  assign passive_if.tx_gasket_DataBusWidth    = DUT.PCS_U.PCS_TX_U.GasKet_U.DataBusWidth;
+  assign passive_if.tx_gasket_MAC_TX_Data     = DUT.PCS_U.PCS_TX_U.GasKet_U.MAC_TX_Data;
+  assign passive_if.tx_gasket_MAC_TX_DataK    = DUT.PCS_U.PCS_TX_U.GasKet_U.MAC_TX_DataK;
+  assign passive_if.tx_gasket_MAC_Data_En     = DUT.PCS_U.PCS_TX_U.GasKet_U.MAC_Data_En; 
+  assign passive_if.tx_gasket_TxDataK         = DUT.PCS_U.PCS_TX_U.GasKet_U.TxDataK;
+  assign passive_if.tx_gasket_TxData          = DUT.PCS_U.PCS_TX_U.GasKet_U.TxData;
+
+////////////// ENCODER /////////////////
+
+  assign passive_if.encoder_data              = DUT.PCS_U.PCS_TX_U.Encoding_U.data;
+  assign passive_if.encoder_MAC_Data_En       = DUT.PCS_U.PCS_TX_U.Encoding_U.MAC_Data_En;
+  assign passive_if.encoder_Bit_Rate_10       = DUT.PCS_U.PCS_TX_U.Encoding_U.Bit_Rate_10;
+  assign passive_if.encoder_Rst               = DUT.PCS_U.PCS_TX_U.Encoding_U.Rst;
+  assign passive_if.encoder_TXDataK           = DUT.PCS_U.PCS_TX_U.Encoding_U.TXDataK; 
+  assign passive_if.encoder_data_out          = DUT.PCS_U.PCS_TX_U.Encoding_U.data_out;
+
+////////////// TX_PMA /////////////////
+
+  assign passive_if.tx_pma_Bit_Rate_Clk_10    = DUT.PMA_U.PMA_TX_U.Bit_Rate_Clk_10;  
+  assign passive_if.tx_pma_Bit_Rate_Clk       = DUT.PMA_U.PMA_TX_U.Bit_Rate_Clk;
+  assign passive_if.tx_pma_Rst_n              = DUT.PMA_U.PMA_TX_U.Rst_n;
+  assign passive_if.tx_pma_Data_in            = DUT.PMA_U.PMA_TX_U.Data_in; 
+  assign passive_if.tx_pma_MAC_Data_En        = DUT.PMA_U.PMA_TX_U.MAC_Data_En; 
+  assign passive_if.tx_pma_TX_Out_P           = DUT.PMA_U.PMA_TX_U.TX_Out_P;   
+  assign passive_if.tx_pma_TX_Out_N           = DUT.PMA_U.PMA_TX_U.TX_Out_N;
+
+////////////// RX_S2p /////////////////
+
+  assign passive_if.rx_s2p_Recovered_Bit_Clk  = DUT.PMA_U.PM_RX_U.serialToparallel.Recovered_Bit_Clk;
+  assign passive_if.rx_s2p_Ser_in             = DUT.PMA_U.PM_RX_U.serialToparallel.Ser_in;
+  assign passive_if.rx_s2p_Rst_n              = DUT.PMA_U.PM_RX_U.serialToparallel.Rst_n;
+  assign passive_if.rx_s2p_RxPolarity         = DUT.PMA_U.PM_RX_U.serialToparallel.RxPolarity;
+  assign passive_if.rx_s2p_Data_Collected     = DUT.PMA_U.PM_RX_U.serialToparallel.Data_Collected;
+
+////////////// RX_CDR /////////////////
+
+  assign passive_if.cdr_rst_n                 = DUT.PMA_U.PM_RX_U.CDRLoopInst.rst_n;  
+  assign passive_if.cdr_clk_0                 = DUT.PMA_U.PM_RX_U.CDRLoopInst.clk_0;
+  assign passive_if.cdr_clk_data              = DUT.PMA_U.PM_RX_U.CDRLoopInst.clk_data;  
+  assign passive_if.cdr_Din                   = DUT.PMA_U.PM_RX_U.CDRLoopInst.Din;
+  assign passive_if.cdr_PI_Clk                = DUT.PMA_U.PM_RX_U.CDRLoopInst.PI_Clk;
+  assign passive_if.cdr_Dout                  = DUT.PMA_U.PM_RX_U.CDRLoopInst.Dout;
+  //...internal 
+  assign passive_if.cdr_code                  = DUT.PMA_U.PM_RX_U.CDRLoopInst.DLF_U.code;
+
+////////////// EBUFFER /////////////////
+
+  assign passive_if.ebuffer_write_clk         = DUT.PCS_U.PCS_RX_U.buffer.write_clk;
+  assign passive_if.ebuffer_read_clk          = DUT.PCS_U.PCS_RX_U.buffer.read_clk;
+  assign passive_if.ebuffer_data_in           = DUT.PCS_U.PCS_RX_U.buffer.data_in;
+  assign passive_if.ebuffer_rst_n             = DUT.PCS_U.PCS_RX_U.buffer.rst_n;
+  assign passive_if.ebuffer_skp_added         = DUT.PCS_U.PCS_RX_U.buffer.skp_added;
+  assign passive_if.ebuffer_Skp_Removed       = DUT.PCS_U.PCS_RX_U.buffer.Skp_Removed;
+  assign passive_if.ebuffer_overflow          = DUT.PCS_U.PCS_RX_U.buffer.overflow;
+  assign passive_if.ebuffer_underflow         = DUT.PCS_U.PCS_RX_U.buffer.underflow;
+  assign passive_if.ebuffer_data_out          = DUT.PCS_U.PCS_RX_U.buffer.data_out;
+
+
+////////////// DECODER /////////////////
+
+  assign passive_if.decoder_CLK               = DUT.PCS_U.PCS_RX_U.decode.CLK;
+  assign passive_if.decoder_Rst_n             = DUT.PCS_U.PCS_RX_U.decode.Rst_n;
+  assign passive_if.decoder_Data_in           = DUT.PCS_U.PCS_RX_U.decode.Data_in;
+  assign passive_if.decoder_Data_out          = DUT.PCS_U.PCS_RX_U.decode.Data_out;
+  assign passive_if.decoder_DecodeError       = DUT.PCS_U.PCS_RX_U.decode.DecodeError;
+  assign passive_if.decoder_DisparityError    = DUT.PCS_U.PCS_RX_U.decode.DisparityError;
+  assign passive_if.decoder_RxDataK           = DUT.PCS_U.PCS_RX_U.decode.RxDataK;
+
+////////////// RX_GASKET ///////////////
+
+  assign passive_if.rx_gasket_clk_to_get      = DUT.PCS_U.PCS_RX_U.rx_gasket.clk_to_get;
+  assign passive_if.rx_gasket_PCLK            = DUT.PCS_U.PCS_RX_U.rx_gasket.PCLK;
+  assign passive_if.rx_gasket_Rst_n           = DUT.PCS_U.PCS_RX_U.rx_gasket.Rst_n;
+  assign passive_if.rx_gasket_Rx_Datak        = DUT.PCS_U.PCS_RX_U.rx_gasket.Rx_Datak;
+  assign passive_if.rx_gasket_width           = DUT.PCS_U.PCS_RX_U.rx_gasket.width;
+  assign passive_if.rx_gasket_Data_in         = DUT.PCS_U.PCS_RX_U.rx_gasket.Data_in;
+  assign passive_if.rx_gasket_Data_out        = DUT.PCS_U.PCS_RX_U.rx_gasket.Data_out;
+
+
+
+  ////////////////////////////////////
+  ////////// PHY ASSERTIONS //////////
+  ////////////////////////////////////
 
   bind DUT phy_assertions #(
       .DATA_BUS_WIDTH(32)
@@ -89,7 +181,7 @@ module top;
       .enable  (DUT.MAC_Data_En)
   );
 
-  ///////////////////// COMMA Assertions ///////////////////
+  /////////// COMMA Assertions ////////
 
   bind DUT.PCS_U.PCS_RX_U.Comma_Detection_U comma_assertions commaAssertion (
       .clk         (DUT.PCS_U.PCS_RX_U.Comma_Detection_U.clk),
@@ -105,9 +197,11 @@ module top;
 
   );
 
-  ///////////////////////////////////////////////////////////////////////////////////
-  /////////////////////////// Elastic Buffer Assertions /////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////////
+
+  ///////////////////////////////////////////////
+  //////////// ELASTIC BUFFER ASSERTIONS ////////
+  ///////////////////////////////////////////////
+
 
   bind DUT.PCS_U.PCS_RX_U.buffer buffer_assertions #(
       .MAXBUFFERADR(4),
@@ -150,6 +244,7 @@ module top;
   initial begin
     uvm_config_db#(virtual BFM_if)::set(null, "*", "bfm_if", dut_if);  // BUS FUNCTIONAL MODEL
     uvm_config_db#(virtual INTERNALS_if)::set(null, "*", "internals_if",internals_if);  // INTERNAL INTERFACE
+    uvm_config_db#(virtual PASSIVE_if)::set(null,"*","passive_if",passive_if);
 
     //------ RUNNING THE TEST --------
     run_test("my_test");
